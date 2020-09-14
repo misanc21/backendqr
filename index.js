672569672283680dc10542ffe37d25bd887d35d7
misanc21 <misanc21@gmail.com>
2020-09-14 13:40:25 -0500

    iniciar servidor node

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.port || 4000;
+
+
+app.listen(port,  () => {
+    console.log('el servidor funciona ggg')
+})
